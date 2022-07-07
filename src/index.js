92953e189a06ef9f6c080d155e345326d19c2e2d
SergeyKuleshov <kilinserg89@mail.ru>
2022-07-07 17:28:37 +0300

    Create engine for all games in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+import readlineSync from 'readline-sync';
+import greetings from './cli.js';
+
 function randomNumber(range) {
   return Math.floor(Math.random() * range);
 }
 
-export default randomNumber;
+function engine(rules, correctAnswers, questions) {
+  const name = greetings();
+  console.log(rules);
+
+  const round = 3;
+  for (let i = 0; i < round; i += 1) {
+    console.log(`${questions[i]}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = correctAnswers[i];
+
+    if (correctAnswer !== answer) {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+    }
+    console.log('Correct!');
+  }
+  return console.log(`Congratulations, ${name}!`);
+}
+
+export { randomNumber, engine };
